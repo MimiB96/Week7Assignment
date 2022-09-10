53b934d426b27d83f1911eaec0fa0d480487cfb7
MimiB96 <109827743+MimiB96@users.noreply.github.com>
2022-09-10 09:23:19 +0100

    Added forecast data

--- src/app.js
+++ src/app.js
@@ -68,8 +68,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
-  let apiKey = "25499f9fd2b3b742a2a887d3d9745f66";
+  let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -101,7 +100,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "25499f9fd2b3b742a2a887d3d9745f66";
+  let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
